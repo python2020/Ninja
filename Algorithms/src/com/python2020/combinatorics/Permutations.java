dcdd858ffac9193fc7a08b800b4bff69a7726df2
python2020 <dongqing.zhu@gmail.com>
2014-06-15 02:34:04 -0400

    rank

--- Algorithms/src/com/python2020/combinatorics/Permutations.java
+++ Algorithms/src/com/python2020/combinatorics/Permutations.java
@@ -1,6 +1,7 @@
 package com.python2020.combinatorics;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -32,5 +33,33 @@ public class Permutations {
 			tmp = num[p]; num[p] = num[j]; num[j] = tmp; // backtrack              
 		}
 	} 
-
+	
+	public static int rank(ArrayList<Integer> perm) {
+		ArrayList<Integer> indexes = (ArrayList<Integer>) perm.clone();
+		Collections.sort(indexes);
+		return rank(perm, indexes, 0);
+	}
+	
+	public static int rank(ArrayList<Integer> perm, ArrayList<Integer> indexes, int p) {	
+		int numLeft = perm.size() - p - 1;
+		if (numLeft == 0) return 0;
+		
+		int index = indexes.indexOf(perm.get(p)); // "rank" of perm.get(p)
+		indexes.remove(index);
+		int fact = 1;
+		
+		while (numLeft > 1) { 
+			fact *= numLeft;
+			numLeft--;
+		}
+		
+		return index * fact + rank(perm, indexes, p+1);
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> perm = new ArrayList<Integer>();
+		int[] a = {2,1,3,4,5};
+		for (int i = 0; i < a.length; i++) perm.add(a[i]);
+		assert(Permutations.rank(perm) == 24);
+	}
 }
