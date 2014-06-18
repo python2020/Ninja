f649bca880d73b6cce592b01ebee09042f793417
python2020 <dongqing.zhu@gmail.com>
2014-06-18 03:15:36 -0400

    d

---
+++ Algorithms/src/com/python2020/dp/DistinctSubsequences.java
@@ -0,0 +1,18 @@
+package com.python2020.dp;
+
+public class DistinctSubsequences {
+	public int numDistinct(String S, String T) {
+		int m = S.length(); 
+		int n = T.length();
+		if (m < n) return 0; 
+
+		int[] f = new int[n+1];
+		f[0] = 1;
+
+		for (int i = 0; i < m; i++) 
+			for (int j = n; j > 0; j--) 
+				f[j] += (S.charAt(i) == T.charAt(j-1)? f[j-1] : 0); 
+
+		return f[n];          
+	}
+}
---
+++ Algorithms/src/com/python2020/strings/RegularExpressionMatching.java
@@ -0,0 +1,36 @@
+package com.python2020.strings;
+
+public class RegularExpressionMatching {
+	public boolean isMatch(String s, String p) {
+		return isMatchedHelp(s, 0, p, 0);
+	}
+
+	public boolean isMatchedHelp(String s, int si, String p, int pi) {
+		int slen = s.length();
+		int plen = p.length();
+		if (pi == plen) {
+			if (si == slen) return true;
+			else return false;
+		}
+
+		char cp = p.charAt(pi);
+
+		if (pi+1 < plen && p.charAt(pi+1) == '*') {
+			pi += 2;
+			if (isMatchedHelp(s, si, p, pi)) return true;
+
+			for (int i = 0; si+i < slen; i++) {
+				if (cp == '.' || cp == s.charAt(si+i)) {
+					if (isMatchedHelp(s, si+i+1, p, pi)) return true;
+				}
+				else break;
+			}
+		}
+		else {
+			if (si == slen) return false;
+			if (cp == '.' || cp == s.charAt(si)) 
+				return isMatchedHelp(s, si+1, p, pi+1);
+		}
+		return false;
+	}
+}
