45f3b2e32893cb1f32bb855701ea4d7e491d9a41
python2020 <dongqing.zhu@gmail.com>
2014-06-23 23:13:44 -0700

    a

---
+++ Algorithms/src/stack/EvaluateInfixPolishNotation.java
@@ -0,0 +1,57 @@
+package stack;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+public class EvaluateInfixPolishNotation {
+	// simple version: single digit
+	public int eval(String exp) {
+		exp = exp + ")"; // trick: for termination
+
+
+		Set<Character> operSet = new HashSet<Character>();
+		char[] opers = {'+', '-', '*', '/'};
+		for (int i = 0; i < opers.length; i++) operSet.add(opers[i]);
+
+		Stack<Integer> vstack = new Stack<Integer>();
+		Stack<Character> ostack = new Stack<Character>();
+
+		int length = exp.length(), f = 0, s = 0;
+		char c;
+		for (int i = 0; i < length; i++) {
+			c = exp.charAt(i);
+
+			if (operSet.contains(c)) {
+				ostack.push(c);
+				//
+			} else if (c == '(') {
+				continue;
+			} else if (c == ')') {
+				s = vstack.pop();
+				f = vstack.pop();
+
+				switch(ostack.pop()) {
+				case '+': vstack.push(f + s); break;
+				case '-': vstack.push(f - s); break;
+				case '*': vstack.push(f * s); break;
+				case '/': vstack.push(f / s); break;
+				default: break;
+				}
+				System.out.println(vstack.peek());
+			} else {
+				vstack.push(c-'0');
+			}
+		}
+
+		return vstack.pop();
+	}
+
+	public static void main(String[] args) {
+		int v = (((2+5)*(6+1))-9)/4;
+		String exp = "(((2+5)*(6+1))-9)/4";
+		EvaluateInfixPolishNotation x = new EvaluateInfixPolishNotation();
+		System.out.println(v);
+		assert(x.eval(exp) == v);
+	}
+}
