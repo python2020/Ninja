623da28eb7fb266fea2b40b6650b625eb56a6a49
python2020 <dongqing.zhu@gmail.com>
2014-06-25 22:24:36 -0700

    dp

--- Algorithms/src/dp/UniquePaths.java
+++ Algorithms/src/dp/UniquePaths.java
@@ -16,4 +16,22 @@ public class UniquePaths {
 
 		return f[n-1];        
 	}
+
+
+	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+		int m = obstacleGrid.length, n = obstacleGrid[0].length;
+		int[] f = new int[n];
+		f[0] = 1 - obstacleGrid[0][0];
+
+		for (int i = 1; i < n; i++) 
+			f[i] = f[i-1] * (1 - obstacleGrid[0][i]);
+
+		for (int i = 1; i < m; i++) {
+			f[0] = f[0] * (1 - obstacleGrid[i][0]); 
+			for (int j = 1; j < n; j++) 
+				f[j] = (f[j-1] + f[j]) * (1 - obstacleGrid[i][j]);   
+		}
+
+		return f[n-1];    
+	}
 }
