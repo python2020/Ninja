ddf8e1dbf5ae5e0e24c297522fae6172d6c4cbd5
python2020 <dongqing.zhu@gmail.com>
2014-06-27 10:51:11 -0700

    c

--- Algorithms/src/lists/RemoveDuplicatesFromSortedList.java
+++ Algorithms/src/lists/RemoveDuplicatesFromSortedList.java
@@ -17,4 +17,27 @@ public class RemoveDuplicatesFromSortedList {
         
         return head;            
     }
+    
+    
+    
+    public ListNode deleteDuplicates2(ListNode head) {
+        ListNode dummy = new ListNode(0), slow = dummy, fast = null;
+        dummy.next = head;
+        
+        int v = 0;
+        while (slow.next != null && slow.next.next != null) {
+            if (slow.next.val != slow.next.next.val) {
+                slow = slow.next;            
+            } else {
+                v = slow.next.val;
+                fast = slow.next.next.next; // skip two elems
+                while (fast != null && fast.val == v) 
+                    fast = fast.next;
+                
+                slow.next = fast;
+            }
+        }    
+        
+        return dummy.next;
+    }
 }
