a9e7d45362c983c789f4a8ba7a81f5f2053e5748
python2020 <dongqing.zhu@gmail.com>
2014-07-18 21:16:27 -0700

    18

--- Algorithms/src/lists/ListNode.java
+++ Algorithms/src/lists/ListNode.java
@@ -3,8 +3,19 @@ package lists;
 public class ListNode {
 	public int val;
 	public ListNode next;	
+	
+//	public ListNode () {
+//		val = 0;
+//		next = null;
+//	}
+	
 	public ListNode (int v) {
 		val = v;
 		next = null;
 	}
+	
+//	public ListNode (int v, ListNode n) {
+//		val = v;
+//		next = n;
+//	}
 }
